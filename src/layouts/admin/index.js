c3218c4715b0b0f74f49c961a1aff8da531961b4
divyendu <divyendua@gmail.com>
2023-12-08 20:33:09 -0500

    fix home page

--- src/layouts/admin/index.js
+++ src/layouts/admin/index.js
@@ -1,6 +1,5 @@
 // Chakra imports
 import { Portal, Box, useDisclosure, Text, Button, Link } from '@chakra-ui/react';
-import Footer from 'components/footer/FooterAdmin.js';
 // Layout components
 import Navbar from 'components/navbar/NavbarAdmin.js';
 import Sidebar from 'components/sidebar/Sidebar.js';
@@ -147,8 +146,7 @@ export default function Dashboard(props) {
 							</Box>
 						) : null}
 						<Box>
-							<Footer />
-						</Box>
+\						</Box>
 					</Box>
 					</SidebarContext.Provider>
 			</Box>
